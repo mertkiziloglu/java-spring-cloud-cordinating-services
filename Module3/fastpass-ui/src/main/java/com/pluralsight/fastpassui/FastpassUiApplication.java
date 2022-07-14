5c0689bee42e1a59a514a0898029a295b18375dd
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-07-15 01:39:01 +0300

    registering multiple services

---
+++ Module3/fastpass-ui/src/main/java/com/pluralsight/fastpassui/FastpassUiApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.fastpassui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastpassUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastpassUiApplication.class, args);
+	}
+
+}
